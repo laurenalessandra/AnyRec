4d5b76414872fedef42fc4a264e82a7dcd4a4ff1
Lauren Simon <laurenalessandra@hotmail.com>
2019-12-29 12:02:12 +0800

    SightDetailViewController

---
+++ AnyRec/SightDetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SightDetailViewController.swift
+//  AnyRec
+//
+//  Created by Lauren Simon on 29.12.19.
+//  Copyright © 2019 Lauren Simon. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import SafariServices
+
+class SightDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
